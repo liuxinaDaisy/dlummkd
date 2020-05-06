d1dbe3da54f96d30a44722262e2a9d54ca5517ca
liuxinaDaisy <liuxinadaisy@gmail.com>
2020-05-06 10:18:05 +0800

    18.03.01

--- network.py
+++ network.py
@@ -163,21 +163,14 @@ class Build_network(object):
             block8_2 = DR_block_leaky(block8_1, wr8_3, wr8_4, keep_prob,  is_train = mode, rate = 2, bn_trainable = bn_collect_trainable, scope = bn_scope + 'bn_8_2')
 
         with tf.variable_scope('group_9') as scope:
-            w9_1 = weight_variable(shape = [3, 3, feature_base * 32, feature_base * 32], trainable = kernel_collect_trainable, name = "Variable")
-            conv9_1 = bn_leaky_relu_conv2d_layer(block8_2, w9_1, keep_prob, is_train = mode, bn_trainable = bn_collect_trainable, scope = bn_scope + 'bn_9_1')
-            w9_2 = weight_variable(shape = [3, 3, feature_base * 32, feature_base * 32], trainable = kernel_collect_trainable, name = "Variable_1")
-            conv9_2 = bn_leaky_relu_conv2d_layer(conv9_1, w9_2, keep_prob, is_train = mode, bn_trainable = bn_collect_trainable, scope = bn_scope + 'bn_9_2')
+            local_size = 8 * 8
+            w9_1 = weight_variable(shape = [3, 3, feature_base * 32, local_size * self.n_class * 8], trainable = kernel_collect_trainable, name = "Variable")
+            conv9_1 = conv2d_sym(block8_2, w9_1, is_train = mode, bn_trainable = bn_collect_trainable, scope = bn_scope + 'bn_9_1')
+            flat_conv9_1 = PS(conv9_1, r = 8, n_channel = self.n_class * 8, batch_size = self.batch_size) # phase shift
 
-        with tf.variable_scope('group_10') as scope:
-            local_size = 8 * 8 # since the input feature is 8* downsampled, therefore, we need to recover corresponding size. In this case 1 pixel in feature space encodes the laber
-            # of a 8*8 region of the original image
-            w10_1 = weight_variable( shape = [3, 3, feature_base * 32, local_size * self.n_class * 8], trainable = kernel_collect_trainable, name = "Variable")
-            conv10_1 = conv2d_sym(conv9_2, w10_1, is_train = mode, bn_trainable = bn_collect_trainable, scope = bn_scope + 'bn_10_1')
-            flat_conv10_1 = PS(conv10_1, r = 8, n_channel = self.n_class * 8, batch_size = self.batch_size) # phase shift
-
-        with tf.variable_scope('output') as scope:
-            w11_1 = weight_variable( shape = [5, 5, self.n_class * 8, self.n_class], trainable = kernel_collect_trainable, name = "Variable")
-            logits = conv2d_sym_only(flat_conv10_1, w11_1)
+        with tf.variable_scope('output') as scope:    
+            w9_2 = weight_variable(shape = [3, 3, self.n_class * 8, self.n_class], trainable = kernel_collect_trainable, name = "Variable")
+            logits = conv2d_sym_only(flat_conv9_1, w9_2)
 
         return logits
 
